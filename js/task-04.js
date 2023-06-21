796c4aee83bd9e221c3ccddde9837abdba2be7eb
SerhiiHolovk <98563452+SerhiiHolovk@users.noreply.github.com>
2023-06-21 23:49:57 +0300

    вирішення четвертого завдання

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+const counterValue = document.getElementById('value');
+const decrementButton = document.querySelector('button[data-action = "decrement"]');
+const incrementButton = document.querySelector('button[data-action = "increment"]');
+
+let currentValue = 0;
+incrementButton.addEventListener('click', () => {
+    currentValue += 1;
+    counterValue.textContent = currentValue;
+});
+
+decrementButton.addEventListener('click', () => {
+    currentValue -= 1;
+    counterValue.textContent = currentValue;
+});
